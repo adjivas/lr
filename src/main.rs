47928cc5bc86c7af3a4be5346c933a3f9d86504b
adjivas <adjivas@users.noreply.github.com>
2016-10-08 03:15:34 +0200

    solve

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     let input_line = String::from("abcdefghijklmnop<^[[1;1Haaaaa");
         
     println!("{:?}", lr::parse_Screen(&input_line.to_string()) );
+
+    let input_line = String::from("aAbcdefghijklmnop<^[[1;1H1A~1aaaa1");
+        
+    println!("{:?}", lr::parse_Screen(&input_line.to_string()) );
 }
